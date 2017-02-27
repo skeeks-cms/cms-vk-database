9788c5f23a0234d2a1fc95a4a2b682eca28b0cbf
Semenov <semenov@skeeks.com>
2017-02-27 20:40:01 +0300

    init project

--- src/config/main-console.php
+++ src/config/main-console.php
@@ -1,4 +1,41 @@
 <?php
 return [
 
+    'components' =>
+    [
+        /*'vkApi' => [
+            'class' => 'jumper423\VK',
+            'clientId' => '11111',
+            'clientSecret' => 'n9wsv98svSD867SA7dsda87',
+            'delay' => 0.7, // Минимальная задержка между запросами
+            'delayExecute' => 120, // Задержка между группами инструкций в очереди
+            'limitExecute' => 1, // Количество инструкций на одно выполнении в очереди
+            'captcha' => 'captcha', // Компонент по распознованию капчи
+        ],*/
+
+        'vkDatabase' => [
+            'class'     => 'skeeks\cms\vkDatabase\VkDatabaseComponent',
+        ],
+
+        'i18n' => [
+            'translations' =>
+            [
+                'skeeks/vkDatabase' => [
+                    'class'             => 'yii\i18n\PhpMessageSource',
+                    'basePath'          => '@skeeks/cms/vkDatabase/messages',
+                    'fileMap' => [
+                        'skeeks/vkDatabase' => 'main.php',
+                    ],
+                ]
+            ]
+        ]
+    ],
+
+    'modules' =>
+    [
+        'vkDatabase' => [
+            'class'                 => 'skeeks\cms\vkDatabase\VkDatabaseModule',
+            'controllerNamespace'   => 'skeeks\cms\vkDatabase\console\controllers'
+        ]
+    ]
 ];
---
+++ src/console/controllers/ImportController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link https://skeeks.com/
+ * @copyright (c) 2010 SkeekS
+ * @date 27.02.2017
+ */
+namespace skeeks\cms\vkDatabase\console\controllers;
+use yii\console\Controller;
+use yii\helpers\Console;
+
+/**
+ * Import data from vk
+ */
+class ImportController extends Controller
+{
+    /**
+     * Import countries
+     */
+    public function actionCountries()
+    {
+
+        $this->stdout("test");
+    }
+}
